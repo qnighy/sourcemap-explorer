2ebd66299fe2a976f17b94677c0f774edaebce21
Masaki Hara <ackie.h.gmai@gmail.com>
2020-10-31 10:53:43 +0900

    Split panes

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,33 @@
-import React, { useCallback, useState } from 'react';
-import { useDropzone, FileRejection, DropEvent } from 'react-dropzone';
+import React from 'react';
+import { useDropzone } from 'react-dropzone';
+import { useUploader } from './Uploader';
 import './App.css';
 
 const App: React.FC = () => {
-  const [uploadedFiles, setUploadedFiles] = useState<Map<string, ArrayBuffer>>(() => new Map());
-  const onDrop = useCallback(async (acceptedFiles: File[], fileRejections: FileRejection[], event: DropEvent) => {
-    for (const file of acceptedFiles) {
-      const content = await file.arrayBuffer();
-      setUploadedFiles((oldState) => {
-        const state = new Map(oldState);
-        state.set(file.name, content);
-        return state;
-      });
-    }
-  }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+  const uploaderState = useUploader();
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop: uploaderState.onDrop})
   return (
     <div className="App">
       <h1>SourceMap Explorer</h1>
-      <ul>
-        {
-          Array.from(uploadedFiles.entries()).map(([name, _content]) => (
-            <li key={name}>{name}</li>
-          ))
-        }
-      </ul>
-      <div {...getRootProps()}>
-        <input {...getInputProps()} />
-        {
-          isDragActive ?
-            <p>Drop the files here ...</p> :
-            <p>Drag 'n' drop some files here, or click to select files</p>
-        }
+      <div className="editor">
+        <div className="editor-generated">
+          <ul>
+            {
+              Array.from(uploaderState.uploadedFiles.entries()).map(([name, _content]) => (
+                <li key={name}>{name}</li>
+              ))
+            }
+          </ul>
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            {
+              isDragActive ?
+                <p>Drop the files here ...</p> :
+                <p>Drag 'n' drop some files here, or click to select files</p>
+            }
+          </div>
+        </div>
+        <div className="editor-source"></div>
       </div>
     </div>
   );
