d44e95e1b7a181fef38e4b054c88ba3712839210
Masaki Hara <ackie.h.gmai@gmail.com>
2020-10-31 09:17:37 +0900

    Type

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
     </div>
   );
-}
+};
 
 export default App;
