fc635699861df0c0cb5a718160a569945847045a
Masaki Hara <ackie.h.gmai@gmail.com>
2020-10-31 09:51:29 +0900

    Use react-dropzone

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useDropzone, FileRejection, DropEvent } from 'react-dropzone';
 import './App.css';
 
 const App: React.FC = () => {
+  const onDrop = useCallback((acceptedFiles: File[], fileRejections: FileRejection[], event: DropEvent) => {
+    console.log("acceptedFiles = ", acceptedFiles);
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
   return (
     <div className="App">
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {
+          isDragActive ?
+            <p>Drop the files here ...</p> :
+            <p>Drag 'n' drop some files here, or click to select files</p>
+        }
+      </div>
     </div>
   );
 };
