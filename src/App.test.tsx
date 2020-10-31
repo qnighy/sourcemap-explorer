0defd2e9bed83a4b99d63ab5ca8dff55f9ff47ef
Masaki Hara <ackie.h.gmai@gmail.com>
2020-10-31 10:19:48 +0900

    Uploader

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const dragElement = screen.getByText(/Drag 'n' drop/i);
+  const dragElement = screen.getByText(/SourceMap Explorer/i);
   expect(dragElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useDropzone, FileRejection, DropEvent } from 'react-dropzone';
 import './App.css';
 
 const App: React.FC = () => {
-  const onDrop = useCallback((acceptedFiles: File[], fileRejections: FileRejection[], event: DropEvent) => {
-    console.log("acceptedFiles = ", acceptedFiles);
+  const [uploadedFiles, setUploadedFiles] = useState<Map<string, ArrayBuffer>>(() => new Map());
+  const onDrop = useCallback(async (acceptedFiles: File[], fileRejections: FileRejection[], event: DropEvent) => {
+    for (const file of acceptedFiles) {
+      const content = await file.arrayBuffer();
+      setUploadedFiles((oldState) => {
+        const state = new Map(oldState);
+        state.set(file.name, content);
+        return state;
+      });
+    }
   }, [])
   const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
   return (
     <div className="App">
+      <h1>SourceMap Explorer</h1>
+      <ul>
+        {
+          Array.from(uploadedFiles.entries()).map(([name, _content]) => (
+            <li key={name}>{name}</li>
+          ))
+        }
+      </ul>
       <div {...getRootProps()}>
         <input {...getInputProps()} />
         {
