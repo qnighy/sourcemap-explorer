a889ab8e508a7294085d87b78328318e5f0565c1
Masaki Hara <ackie.h.gmai@gmail.com>
2020-11-01 11:14:32 +0900

    Parse sourcemap

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,22 @@ import './App.css';
 import { parseFiles, ParseResult } from './parse';
 import { useDiffMemo } from './diff_memo';
 
+interface UnmappedSegment {
+  column: number;
+  source?: undefined;
+  sourceLine?: undefined;
+  sourceColumn?: undefined;
+  name?: undefined;
+}
+interface MappedSegment {
+  column: number;
+  source: string;
+  sourceLine: number;
+  sourceColumn: number;
+  name?: string;
+}
+type Segment = UnmappedSegment | MappedSegment;
+
 const App: React.FC = () => {
   const uploaderState = useUploader();
   const parseResult = useDiffMemo((prev?: ParseResult) => parseFiles(uploaderState.uploadedFiles, prev), [uploaderState.uploadedFiles]);
@@ -15,6 +31,70 @@ const App: React.FC = () => {
 
   const [selectedGenerated, setSelectedGenerated] = useState<string | undefined>(undefined);
   const selectedGeneratedFile = selectedGenerated !== undefined ? uploaderState.uploadedFiles.get(selectedGenerated) : undefined;
+  if (selectedGeneratedFile) {
+    // TODO: relative path
+    const selectedGeneratedParsed = selectedGenerated !== undefined ? parseResult.files.get(selectedGenerated) : undefined;
+    if (selectedGeneratedParsed && selectedGeneratedParsed.sourceMapRef) {
+      const mapFile = parseResult.files.get(selectedGeneratedParsed.sourceMapRef);
+      if (mapFile && mapFile.sourceMap) {
+        const mappings = mapFile.sourceMap.mappings + ";";
+        const lines: Segment[][] = [];
+        let segments: Segment[] = [];
+        let lastColumn = 0;
+        let lastSourceIndex = 0;
+        let lastSourceLine = 0;
+        let lastSourceColumn = 0;
+        let lastNameIndex = 0;
+        let currentSegment: number[] = [];
+        let current = 0;
+        let currentBits = 0;
+        for (let i = 0; i < mappings.length; i++) {
+          const charCode = mappings.charCodeAt(i);
+          if (charCode === 0x3B /* ; */ || charCode === 0x2C /* , */) {
+            if (current !== 0) throw new Error("VLQ runover");
+            if (currentSegment.length === 0) continue;
+            // TODO: check monotonicity
+            lastColumn += toSigned(currentSegment[0]);
+            if (currentSegment.length === 4 || currentSegment.length === 5) {
+              lastSourceIndex += toSigned(currentSegment[1]);
+              lastSourceLine += toSigned(currentSegment[2]);
+              lastSourceColumn += toSigned(currentSegment[3]);
+              if (currentSegment.length === 5) {
+                lastNameIndex += toSigned(currentSegment[4]);
+              }
+            } else if (currentSegment.length !== 1) {
+              throw new Error("Invalid segment length");
+            }
+            segments.push({
+              column: lastColumn,
+              // TODO: check index
+              source: mapFile.sourceMap.sources[lastSourceIndex],
+              sourceLine: lastSourceLine,
+              sourceColumn: lastSourceColumn,
+              name: currentSegment.length === 5 ? mapFile.sourceMap.names[lastNameIndex] : undefined,
+            });
+            currentSegment = [];
+            if (charCode === 0x3B /* ; */) {
+              lines.push(segments);
+              segments = [];
+              lastColumn = 0;
+            }
+            continue;
+          }
+          const b = base64val(charCode);
+          if (b < 32) {
+            currentSegment.push(current | (b << currentBits));
+            current = 0;
+            currentBits = 0;
+          } else {
+            current |= (b << (currentBits & 31));
+            currentBits += 5;
+          }
+        }
+        console.log(lines);
+      }
+    }
+  }
   return (
     <div className="App">
       <h1>SourceMap Explorer</h1>
@@ -90,5 +170,29 @@ const FileListEntry: React.FC<FileListEntryProps> = (props) => {
     </li>
   );
 };
+
+const base64val = (charCode: number): number => {
+  if (charCode >= 0x41 /* A */ && charCode <= 0x5A /* Z */) {
+    return charCode - 0x41;
+  } else if (charCode >= 0x61 /* a */ && charCode <= 0x7A /* z */) {
+    return charCode - (0x61 - 26);
+  } else if (charCode >= 0x30 /* 0 */ && charCode <= 0x39 /* 9 */) {
+    return charCode + (52 - 0x30);
+  } else if (charCode === 0x2B /* + */) {
+    return 62;
+  } else if (charCode === 0x2F /* / */) {
+    return 63;
+  } else {
+    throw new Error(`Invalid base64 value: ${charCode}`);
+  }
+};
+
+const toSigned = (n: number): number => {
+  if (n & 1) {
+    return ~(n >> 1);
+  } else {
+    return (n >> 1);
+  }
+}
 
 export default App;
